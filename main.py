68755a7ba2dac1fc357651ec3ae5ffd165e24f25
MaflowCraft <71622427+MAFDevs@users.noreply.github.com>
2022-12-08 14:39:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import discord, os, keep_alive, asyncio, datetime, pytz
-
-
 from discord.ext import tasks, commands
 
 client = commands.Bot(
@@ -8,11 +6,9 @@ client = commands.Bot(
   self_bot=True
 )
 
-
-
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = " HamsShop ┊ Store Lengkap & Termurah ┊ 24/7 Online ┊ https://dsc.gg/hamsshop ", url = "link twicth"))
+  await client.change_presence(activity = discord.Streaming(name = " text here ", url = "link twicth"))
 
 
 keep_alive.keep_alive()
